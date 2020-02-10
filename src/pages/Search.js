57b69c1aeddfd6b772b615fb22bfd64d0bd62439
Rob Hess <robwhess@gmail.com>
2020-02-10 15:36:52 -0800

    Add code from 2/10 lecture.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import fetch from 'isomorphic-unfetch';
 
 import { Input, Button } from '../components/FormComponents';
+import Spinner from '../components/Spinner';
 
 function Search({ query }) {
   const [ inputQuery, setInputQuery ] = useState(query || "");
+  const [ repos, setRepos ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
   const history = useHistory();
+
+  useEffect(() => {
+    if (query) {
+      let ignore = false;
+      const controller = new AbortController();
+
+      async function fetchSearchResults() {
+        let responseBody = {};
+        setLoading(true);
+        try {
+          const response = await fetch(
+            `https://api.github.com/search/repositories?q=${query}&sort=stars`,
+            { signal: controller.signal }
+          );
+          responseBody = await response.json();
+        } catch (e) {
+          if (e instanceof DOMException) {
+            console.log("== HTTP request aborted");
+          } else {
+            throw e;
+          }
+        }
+
+        if (!ignore) {
+          setLoading(false);
+          setRepos(responseBody.items);
+        } else {
+          console.log("== ignoring results");
+        }
+        // console.log("== response body:", responseBody);
+      }
+
+      fetchSearchResults();
+      return () => {
+        controller.abort();
+        ignore = true;
+      };
+    }
+  }, [ query ]);
 
   return (
     <div>
@@ -13,10 +56,23 @@ function Search({ query }) {
         e.preventDefault();
         history.push(`?q=${inputQuery}`);
       }}>
-        <Input value={inputQuery} onChange={e => setInputQuery(e.target.value)} />
+        <Input
+          value={inputQuery}
+          onChange={e => setInputQuery(e.target.value)}
+        />
         <Button type="submit">Search</Button>
       </form>
-      <h2>Search query: {query}</h2>
+      {loading ? (
+        <Spinner />
+      ) :
+        <ul>
+          {repos.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.html_url}>{repo.full_name}</a>
+            </li>
+          ))}
+        </ul>
+      }
     </div>
   );
 }
